83dc9d1fff4a6e144d322401166b49d023eca71e
Po_Chih <okmijn7980@gmail.com>
2021-05-18 11:04:26 +0800

    rot13 a~z

---
+++ rot.py
@@ -0,0 +1,11 @@
+def rot13(word):
+    output =[]
+    for i in word.lower():
+        new_ord = ord(i) + 13
+        if new_ord > ord('z'):
+            new_ord -= 26
+        output.append(chr(new_ord))
+    return ''.join(output)
+
+if __name__ == '__main__':
+    print(rot13('apple'))
