1436fbdae188c31001a583104954f88af6794942
Po_Chih <okmijn7980@gmail.com>
2021-05-19 02:43:49 +0800

    {**} can directly accept also can use .update()2

--- list_tuple.py
+++ list_tuple.py
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
 d1 = {'A': 1,'B': 2}
 d2 = {'C': 3}
 print({**d1, **d2})
 d1.update(d2)
 print(d1)
+=======
+def mysum(*item):
+    if not item:
+        return item
+    output = item[0]
+    for item in item[1:]:
+        if isinstance(item, dict):
+            output.update(item)
+        else:
+            output += item
+    return output
+if __name__ == '__main__':
+    print(mysum({'a': 1, 'b': 2},{'c': 3}))
