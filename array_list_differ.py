a6fc1f853633f194cc03feadef165d6e9cf3a40f
Po_Chih <okmijn7980@gmail.com>
2021-05-18 16:38:18 +0800

    tuple and list have different way to use memory

---
+++ array_list_differ.py
@@ -0,0 +1,9 @@
+import sys
+
+mylist = []
+
+for i in range(15):
+    l_size = sys.getsizeof(mylist)
+    t_size = sys.getsizeof(tuple(mylist))
+    print(f'len = {len(mylist)}, list size = {l_size}, tuple size = {t_size}')
+    mylist.append(i)
