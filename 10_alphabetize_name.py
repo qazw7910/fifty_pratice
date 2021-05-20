506ff602ed5cf53d7909e24a4b7115ba2f77e7db
Po_Chih <okmijn7980@gmail.com>
2021-05-20 09:03:21 +0800

    sorted = (X, key = lambda d: f = d()

---
+++ 10_alphabetize_name.py
@@ -0,0 +1,10 @@
+people = [
+    ('Joe', 'Biden', 'president@usa.gov'),
+    ('Emmanuel', 'Macron', 'president@france.gov'),
+    ('Justin', 'Trudeau', 'primeminister@canada.gov'),
+    ('Angela', 'Merkel', 'primeminister@germany.gov'),
+    ('Jacinda', 'Ardern', 'primeminister@newzealand.gov')
+    ]
+
+for person in sorted(people, key= lambda d : (d[1], d[0])):
+    print(f'{person[1]}, {person[0]} : {person[2]}')
--- list_tuple.py
+++ list_tuple.py
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
+''''<<<<<<< HEAD
 d1 = {'A': 1,'B': 2}
 d2 = {'C': 3}
 print({**d1, **d2})
 d1.update(d2)
 print(d1)
 =======
+'''
 def mysum(*item):
     if not item:
         return item
