a98ad803060c40d7f94f921dec7e5b26d098d3e1
Po_Chih <okmijn7980@gmail.com>
2021-05-20 16:16:03 +0800

    resort acorrding grades also use format to separate word

--- 12_grades_sorted.py
+++ 12_grades_sorted.py
@@ -13,9 +13,11 @@ def output_grades():
     grades.sort(key=operator.itemgetter(2), reverse=True)
     output = []
     for name1, name2, grade in grades:
-        output.append(f'{name1:12s}{name2:10s}{grade:.1f}')
-    return
-
-
+        output.append(f'{name1:12s}{name2:10s}{grade:5.1f}')
+    return '\n'.join(output)
+    '''for grade in grades:
+        output.append(f'{1:12s}{0:8s}{2:.1f}'.format(*grade))
+    return '\n'.join(output)
+    '''
 if __name__ == '__main__':
     print(output_grades())
