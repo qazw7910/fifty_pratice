ddef0043f2c7fc5c8ab248e2663f58ef431d7624
qazw7910 <okmijn7980@gmail.com>
2021-06-08 05:40:55 +0800

    xml have no idea

---
+++ 25_myxml.py
@@ -0,0 +1,12 @@
+def myxml(tag, content='', **kwargs):
+    attrs_list = []
+    for key, value in kwargs.items():
+        attrs_list.append(f' {key} = "{value}"')
+
+    attrs = ''.join(attrs_list)
+
+    return f'<{tag}{attrs}>{content}</{tag}>'
+
+
+if __name__ == '__main__':
+    print(myxml('foo', 'bar', a=1, b=2, c=3))
