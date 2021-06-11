31de243f4870d4fd293b92d0dbdfc0c77536c2c1
qazw7910 <okmijn7980@gmail.com>
2021-06-11 23:58:06 +0800

    string module

---
+++ 35_gematria_dict.py
@@ -0,0 +1,11 @@
+import string
+
+'''print(string.ascii_lowercase)
+print(list(enumerate(string.ascii_lowercase)))
+print(list(enumerate(string.ascii_lowercase, 1)))'''
+def gematria_dict():
+    return {char: index
+            for index, char
+            in enumerate(string.ascii_lowercase, 1)}
+if __name__ == '__main__':
+    print(gematria_dict())
