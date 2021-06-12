3bf68c4e299710eeb1a46adc33bea444316421b8
qazw7910 <okmijn7980@gmail.com>
2021-06-13 02:23:48 +0800

    string module string.ascii_lowercase

--- 35_gematria_dict.py
+++ 35_gematria_dict.py
@@ -4,8 +4,7 @@ print(string.ascii_lowercase)
 print(list(enumerate(string.ascii_lowercase)))
 print(list(enumerate(string.ascii_lowercase, 1)))
 def gematria_dict():
-    return {char: index
-            for index, char
-            in enumerate(string.ascii_lowercase, 1)}
+    return {index : value
+            for value, index in enumerate(string.ascii_lowercase, 1)}
 if __name__ == '__main__':
     print(gematria_dict())
