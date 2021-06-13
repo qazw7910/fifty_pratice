ec2a4945a74ba20e99a5eff688aec2876c138efa
qazw7910 <okmijn7980@gmail.com>
2021-06-13 09:51:18 +0800

    menu option

---
+++ menu.py
@@ -0,0 +1,20 @@
+def menu(**options):
+    def menu_selecter():
+        option_string = '/'.join(options)
+        while True:
+            choice = input(f'選擇項目({option_string}): ')
+            if choice in options:
+                return options[choice]
+                break
+            print('選項不存在!')
+    return menu_selecter()
+
+if __name__ == '__main__':
+    import operator
+
+    a, b = 10, 3
+    my_menu = menu(add = operator.add,
+                   sub = operator.sub,
+                   mul = operator.mul,
+                   div = operator.truediv)
+    print(my_menu()(a, b))
