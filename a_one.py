726e0c2a586792a731149efc49ac604ee5685d73
qazw7910 <okmijn7980@gmail.com>
2021-12-13 14:17:02 +0800

    sum of two use combination to replace for

--- a_one.py
+++ a_one.py
@@ -1,15 +1,21 @@
 from  itertools import  combinations
 
 def sum_of_two(data, k):
-    for a_index, a_value in enumerate(data):
-        for b_index, b_value in enumerate(data):
+    # for a_index, a_value in enumerate(data):
+    #     for b_index, b_value in enumerate(data):
+    #
+    #         if a_index != b_index and a_value + b_value == k:
+    #
+    #             return [a_index, b_index]
+    #
+    #     return []
 
-            if a_index != b_index and a_value + b_value == k:
-
-                return [a_index, b_index]
-
-        return []
+    for a, b in combinations(enumerate(data), 2):
+        if a[1] + b[1] == k :
+            return [a[0] , b[0]]
+    return []
 
 
 if __name__ == '__main__':
-    print(sum_of_two([2,7,11,15],9))
+    print(sum_of_two([2,7,11,15],10))
+
