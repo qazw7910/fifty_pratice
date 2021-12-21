5eb56da8e774ba4a88daf5ed77d06c04b3f033dc
qazw7910 <okmijn7980@gmail.com>
2021-12-21 18:09:30 +0800

    None

--- 13_repeat_word.py
+++ 13_repeat_word.py
@@ -2,18 +2,24 @@ import operator
 from collections import Counter
 #兩種方法
 
-def most_repeat_word(word):
+def most_repeat_word1(word):
     letters = list(set(word))
-    letter_count = []
+    letters_count = []
     for letter in letters:
-        letter_count.append((letter, word.count(letter)))
-    result = sorted(letter_count, key=operator.itemgetter(1))[-1]
-    return f'{result[0]}重複了{result[1]}次'
+        letters_count.append((letter, word.count(letter)))
+
+    result = sorted(letters_count, key=operator.itemgetter(1))[-1]
+    return f'{result[0]} 重複了 {result[1]} 次'
 
 def most_repeat_word2(word):
-        result = Counter(word).most_common(1)[0]
-        print(f'{result[0]}重複了{result[1]}變')
+    result = Counter(word).most_common(1)[0]
+    return f'{result[0]}重複了{result[1]}變'
+
+def most_repeat_word3(word):
+    letter_count = Counter(word)
+    result = letter_count.most_common()[0]
+    return f'{result[0]}'
 
 if __name__ == '__main__':
 
-    print(most_repeat_word2('independence'))
+    print(most_repeat_word3('independence'))
