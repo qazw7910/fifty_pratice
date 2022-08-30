9f999d8838831aa27cf5ee87ffbfc6dbd1a1d623
qazw7910 <okmijn7980@gmail.com>
2022-08-30 16:48:10 +0800

    848

---
+++ leetcode/string/Shifting Letters.py
@@ -0,0 +1,17 @@
+def main():
+    so = Solution()
+    print(so.shiftingLetters(s="abc", shifts=[3, 5, 9]))
+
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: list[int]) -> str:
+        sumi = sum(shifts)
+        L = list(map(ord, list(s)))
+        for i in range(len(L)):
+            L[i] = (L[i] - ord('a') + sumi) % 26 + ord('a')
+            sumi -= shifts[i]
+        return ''.join(map(chr, L))
+
+
+if __name__ == '__main__':
+    main()
