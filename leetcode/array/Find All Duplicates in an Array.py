62392969adde6e053d7ba1a3cab9639c3a5b4621
qazw7910 <okmijn7980@gmail.com>
2022-08-30 15:20:47 +0800

    442

---
+++ leetcode/array/Find All Duplicates in an Array.py
@@ -0,0 +1,19 @@
+def main():
+    so = Solution()
+    print(so.findDuplicates([4, 5, 2, 7, 8, 2, 3, 1]))
+
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        res = []
+        for i in range(len(nums)):
+            ind = abs(nums[i]) - 1
+            if nums[ind] < 0:
+                res.append(ind + 1)
+            else:
+                nums[ind] *= -1
+        return res
+
+
+if __name__ == '__main__':
+    main()
