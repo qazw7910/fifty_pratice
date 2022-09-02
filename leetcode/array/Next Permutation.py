c2999a3acc674ad1c89fe27d1c76dd2011c9c614
qazw7910 <okmijn7980@gmail.com>
2022-09-03 03:22:27 +0800

    31

---
+++ leetcode/array/Next Permutation.py
@@ -0,0 +1,29 @@
+def main():
+    so = Solution()
+    print(so.nextPermutation([1, 2, 3, 4, 6, 3, 1]))
+
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        i = j = len(nums) - 1
+        while nums[i] <= nums[i - 1] and i >= 0:
+            i -= 1
+        if i == 0:
+            nums.reverse()
+            return nums
+
+        k = i - 1
+        while nums[k] > nums[j]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]
+
+        l = k + 1
+        r = len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        return nums
+
+
+if __name__ == '__main__':
+    main()
