64f69f4f1adff0f7ed43e0fefbb9a22cb07e57ca
qazw7910 <okmijn7980@gmail.com>
2022-09-05 03:17:52 +0800

    48 rotate array

---
+++ leetcode/array/Rotate Image.py
@@ -0,0 +1,22 @@
+def main():
+    so = Solution()
+    print(so.rotate(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        l = 0
+        r = len(matrix) - 1
+        while l < r:
+            matrix[l], matrix[r] = matrix[r], matrix[l]
+            l += 1
+            r -= 1
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        return matrix
+
+
+if __name__ == '__main__':
+    main()
