f5c922392253b475b6784d72ded534fb69df1875
qazw7910 <okmijn7980@gmail.com>
2022-10-03 01:50:35 +0800

    8

---
+++ leetcode/string/String Compression.py
@@ -0,0 +1,30 @@
+def main():
+    so = Solution()
+    print(so.compress(["a", "a", "b", "b", "c", "c", "c"]))
+
+
+class Solution:
+    def compress(self, chars: list[str]) -> int:
+        walker, runner = 0, 0
+        while runner < len(chars):
+
+            chars[walker] = chars[runner]
+            count = 1
+
+            while runner + 1 < len(chars) and chars[runner] == chars[runner + 1]:
+                runner += 1
+                count += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[walker + 1] = c
+                    walker += 1
+
+            runner += 1
+            walker += 1
+
+        return walker
+
+
+if __name__ == '__main__':
+    main()
