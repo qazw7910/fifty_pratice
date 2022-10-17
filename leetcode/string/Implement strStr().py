b6c70bc9b9e8dd1814c13c036e13880306ef3ac1
qazw7910 <okmijn7980@gmail.com>
2022-10-18 01:16:26 +0800

    28

---
+++ leetcode/string/Implement strStr().py
@@ -0,0 +1,15 @@
+def main():
+    so = Solution()
+    print(so.strStr(haystack="a", needle="a"))
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle)+1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
+
+
+if __name__ == '__main__':
+    main()
