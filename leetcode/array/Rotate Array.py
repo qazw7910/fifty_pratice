4586b443564b48a4454b601d307ce8854d7f68c2
qazw7910 <okmijn7980@gmail.com>
2022-10-19 02:24:18 +0800

    leetcode189

---
+++ leetcode/array/Rotate Array.py
@@ -0,0 +1,14 @@
+def main():
+    so = Solution()
+    print(so.rotate(nums=[1, 2, 3, 4, 5, 6, 7], k=3))
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        if nums is not None and k > 0:
+            k = k % len(nums)
+            nums[:] = nums[-k:] + nums[:-k]
+            return nums
+
+if __name__ == '__main__':
+    main()
