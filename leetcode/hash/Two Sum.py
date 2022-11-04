53adbd484e09d812469a5e33f330306045897a08
陳柏志 <okmijn7980@gmail.com>
2022-11-05 05:13:35 +0800

    leetcode 1

---
+++ leetcode/hash/Two Sum.py
@@ -0,0 +1,16 @@
+def main():
+    so = Solution()
+    print(so.twoSum(nums=[3, 2, 4], target=6))
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        for key, value in enumerate(nums):
+            a = target - value
+            if a in seen:
+                return [seen[a], key]
+
+            seen[value] = key
+if __name__ == '__main__':
+    main()
