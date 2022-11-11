414e394e691e492af9eabb66c2d4c45762cb98ae
qazw7910 <okmijn7980@gmail.com>
2022-11-12 05:26:59 +0800

    leetcode 1002

---
+++ leetcode/hash/Find Common Characters.py
@@ -0,0 +1,18 @@
+def main():
+    so = Solution()
+    print(so.commonChars(words = ["bella","label","roller"]))
+
+class Solution:
+    def commonChars(self, words:list[str])->list[str]:
+
+        check = list(words[0])
+        for word in words[1:]:
+            newCheck = []
+            for ch in word:
+                if ch in check:
+                    newCheck.append(c)
+                    check.remove(c)
+            check = sorted(newCheck)
+        return check
+if __name__ == '__main__':
+    main()
