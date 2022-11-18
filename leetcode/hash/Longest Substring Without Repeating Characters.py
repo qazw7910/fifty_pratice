96176b215d08e57f5d4a7c7a02acf06ff41a53d5
陳柏志 <okmijn7980@gmail.com>
2022-11-18 19:35:33 +0800

    leetcode 3

---
+++ leetcode/hash/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,12 @@
+def main():
+    so = Solution()
+    print(so.lengthOfLongestSubstring(s="abcabcbb"))
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
+
+
+if __name__ == '__main__':
+   main()
