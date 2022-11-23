7105e8733a5ee0e5369787a4d48e17b1fd85219d
陳柏志 <okmijn7980@gmail.com>
2022-11-24 04:51:58 +0800

    leetcode 3 use two pointers solution

--- leetcode/hash/Longest Substring Without Repeating Characters.py
+++ leetcode/hash/Longest Substring Without Repeating Characters.py
@@ -1,12 +1,42 @@
 def main():
-    so = Solution()
-    print(so.lengthOfLongestSubstring(s="abcabcbb"))
+    so = Solution2()
+    print(so.lengthOfLongestSubstring(s="pwwkew"))
 
 
-class Solution:
+class Solution1:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        start = 0
+        maxLength = 0
+        usedChars = {}
+
+        for i in range(len(s)):
+            if s[i] in usedChars and start <= usedChars[s[i]]:
+                start = usedChars[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChars[s[i]] = i
+
+        return maxLength
+
+
+class Solution2:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        output = 0
+        seen = {}
+        for r in range(len(s)):
+            if s[r] not in seen:
+                output = max(output, r - l + 1)
+            else:
+                if seen[s[r]] < l:
+                    output = max(output, r - l + 1)
+                else:
+                    l = seen[s[r]]+ 1
+            seen[s[r]] = r
+        return output
+
 
 
 if __name__ == '__main__':
-   main()
+    main()
