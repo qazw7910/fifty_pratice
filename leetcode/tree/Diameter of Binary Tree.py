cd3e3a118088ce1e58531e2ed9c1492bacf5b19e
qazw7910 <okmijn7980@gmail.com>
2023-04-02 04:11:49 +0800

    Diameter of Binary Tree.py

--- leetcode/tree/Diameter of Binary Tree.py
+++ leetcode/tree/Diameter of Binary Tree.py
@@ -1,49 +1,40 @@
 def main():
     root = TreeNode(1)
-    root.insertLeft(2)
-    root.insertRight(3)
-    root.left.insertLeft(4)
-    root.left.insertRight(5)
-
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
     so = Solution()
-    print(so.diameterOfBinaryTree(root))
+    result = so.diameterOfBinaryTree(root)
+    print(result)
 
 
 class TreeNode:
-    def __init__(self, value=0, left=None, right=None):
-        self.value = value
+    def __init__(self, root=0, left=None, right=None):
+        self.root = root
         self.left = left
         self.right = right
 
 
-    def insertLeft(self, value):
-        if self.left is None:
-            self.left = TreeNode(value)
-        else:
-            self.left.insertLeft(value)
-
-    def insertRight(self, value):
-        if self.right is None:
-            self.right = TreeNode(value)
-        else:
-            self.right.insertRight(value)
-
-
-
 class Solution:
-    def diameterOfBinaryTree(self, root) -> int:
-        self.ans = 0
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.diameter = 0
 
-        def depth(p):
-            if not p:
+        def max_depth(node):
+            if node is None:
                 return 0
-            left = depth(p.left)
-            right = depth(p.right)
-            self.ans = max(self.ans, left + right)
-            return 1 + max(left, right)
 
-        depth(root)
-        return self.ans
+            left_depth = max_depth(node.left)
+            right_depth = max_depth(node.right)
+
+            self.diameter = max(self.diameter, left_depth + right_depth)
+
+            return 1 + max(left_depth, right_depth)
+
+        max_depth(root)
+
+        return self.diameter
+
 
 if __name__ == '__main__':
     main()
