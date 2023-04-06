67d1878dd1e4862ff29d0f8d2c6d8ba61dfdccc5
陳柏志 <okmijn7980@gmail.com>
2023-04-06 17:14:04 +0800

    mode indent

--- leetcode/array/Container With Most Water.py
+++ leetcode/array/Container With Most Water.py
@@ -9,17 +9,21 @@ class Solution:
         max_area = 0
         # initialize the left and right pointers
         left, right = 0, len(height) - 1
+
         # loop until the two pointers meet
         while left < right:
             area = min(height[left], height[right]) * (right - left)  # calculate the area
             max_area = max(max_area, area)  # update the maximum area found so far
+
             # move the pointer with the smaller height to the right
             if height[left] < height[right]:
                 left += 1
+
             # move the pointer with the smaller height to the left
             else:
                 right -= 1
-        return max_area  # return the maximum area
+
+        return max_area
 
 
 if __name__ == '__main__':
