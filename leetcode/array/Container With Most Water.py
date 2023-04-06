979dcf9571bb29ba4bc2778031655b14c02dae87
陳柏志 <okmijn7980@gmail.com>
2023-04-06 16:49:36 +0800

    leetcode11 Container With Most Water

--- leetcode/array/Container With Most Water.py
+++ leetcode/array/Container With Most Water.py
@@ -5,18 +5,21 @@ def main():
 
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        l = 0
-        r = len(height) - 1
-        width = len(height) - 1
-        res = 0
-        for w in range(width, 0, -1):
-            if height[l] < height[r]:
-                res = max(res, height[l] * w)
-                l += 1
+        # initialize the maximum area to be 0
+        max_area = 0
+        # initialize the left and right pointers
+        left, right = 0, len(height) - 1
+        # loop until the two pointers meet
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)  # calculate the area
+            max_area = max(max_area, area)  # update the maximum area found so far
+            # move the pointer with the smaller height to the right
+            if height[left] < height[right]:
+                left += 1
+            # move the pointer with the smaller height to the left
             else:
-                res = max(res, height[r] * w)
-                r -= 1
-        return res
+                right -= 1
+        return max_area  # return the maximum area
 
 
 if __name__ == '__main__':
