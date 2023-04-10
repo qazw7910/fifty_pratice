0ef86dca4b9e8a3ffc643dc9adac8ce68292e0ff
陳柏志 <okmijn7980@gmail.com>
2023-04-10 17:33:13 +0800

    new solution version

--- leetcode/array/Container With Most Water.py
+++ leetcode/array/Container With Most Water.py
@@ -8,12 +8,12 @@ class Solution:
         # initialize the maximum area to be 0
         max_area = 0
         # initialize the left and right pointers
-        left, right = 0, len(height) - 1
+        left, right = 0, len(height)-1
 
         # loop until the two pointers meet
         while left < right:
-            area = min(height[left], height[right]) * (right - left)  # calculate the area
-            max_area = max(max_area, area)  # update the maximum area found so far
+            area = min(height[left], height[right]) * (right - left)              # calculate the area
+            max_area = max(max_area, area)              # update the maximum area found so far
 
             # move the pointer with the smaller height to the right
             if height[left] < height[right]:
@@ -22,7 +22,6 @@ class Solution:
             # move the pointer with the smaller height to the left
             else:
                 right -= 1
-
         return max_area
 
 
