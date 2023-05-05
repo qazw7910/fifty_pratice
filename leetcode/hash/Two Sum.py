93b8a651b9e15665f653d086288a0a0c0e449deb
qazw7910 <okmijn7980@gmail.com>
2023-05-05 23:37:32 +0800

    hashmap and two pinter solution

--- leetcode/hash/Two Sum.py
+++ leetcode/hash/Two Sum.py
@@ -1,17 +1,31 @@
 def main():
     so = Solution()
-    print(so.twoSum(nums=[3, 2, 4], target=6))
+    print(so.twoSum(nums=[2, 7, 11, 15], target=26))
 
 
 class Solution:
+    # solution1 hashmap
+    # def twoSum(self, nums: list[int], target: int) -> list[int]:
+    #     look_up = {}
+    #
+    #     for index, number in enumerate(nums):
+    #         if target - number in look_up:
+    #             return [look_up[target - number], index]
+    #         else:
+    #             look_up[number] = index
+
+    # solution2 two pointers
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i in range(len(nums)):
-            a = target - nums[i]
-            if a in seen:
-                return [seen[a], i]
+        sorted_nums = sorted(zip(nums, range(len(nums))))
+        left, right = 0, len(nums) - 1
+        while left < right:
+            value = sorted_nums[left][0] + sorted_nums[right][0]
+            if value == target:
+                return [sorted_nums[left][1], sorted_nums[right][1]]
+            elif value < target:
+                left += 1
             else:
-                seen[nums[i]] = i
+                right -= 1
 
 
 if __name__ == '__main__':
