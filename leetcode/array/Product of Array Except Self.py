9f5eef27053fed95f34076a9da5428e2a260b990
qazw7910 <okmijn7980@gmail.com>
2023-05-08 21:16:43 +0800

    leetcode 238

---
+++ leetcode/array/Product of Array Except Self.py
@@ -0,0 +1,39 @@
+import math
+import time
+from typing import List
+
+
+def main():
+    so = Solution()
+    start = time.time()
+    print(so.productExceptSelf(nums=[1, 2, 3, 4]))
+    end = time.time()
+    print((end - start))
+
+
+class Solution:
+    # solution1 time limit
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     output = [1] * len(nums)
+    #     # [1, 1, 1, 1]
+    #     for i in range(len(nums)):
+    #         output[i] = math.prod(nums[:i]) * math.prod(nums[i+1:])
+    #     return output
+    # ----------------------------------------------------------------
+    # solution2
+    def productExceptSelf(self, nums: List[int]) -> List[int]: # TC:O(n) SC:O(1)
+        output = [1] * len(nums)
+        # [1, 1, 1, 1]
+        for i in range(1, len(nums)):  # l->r   TC:O(n)
+            output[i] = output[i - 1] * nums[i - 1]
+
+        right_prod = nums[-1]
+
+        for i in range(len(nums) - 2, -1, -1):  # r->l  TC:O(n)
+            output[i] = right_prod
+            right_prod *= nums[i]
+        return output
+
+
+if __name__ == '__main__':
+    main()
