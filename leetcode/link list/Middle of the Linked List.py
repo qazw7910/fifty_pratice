37a354c00554443dcd72089db9897ad739cc30a1
qazw7910 <okmijn7980@gmail.com>
2023-05-10 16:57:57 +0800

    mode head use iteration to creat ListNode

--- leetcode/link list/Middle of the Linked List.py
+++ leetcode/link list/Middle of the Linked List.py
@@ -2,18 +2,13 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 # create the first node
 head = ListNode(1)
-
-# create the second node and link it to the first node
-head.next = ListNode(2)
-
-# create the third node and link it to the second node
-head.next.next = ListNode(3)
-
-head.next.next.next = ListNode(4)
-
-head.next.next.next.next = ListNode(5)
+current_node = head
+for i in range(2, 6):
+    current_node.next = ListNode(i)
+    current_node = current_node.next
 
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
