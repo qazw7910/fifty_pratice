390812bbbd581bd8d6e9c7dde22d1e9ab0c3a3fa
qazw7910 <okmijn7980@gmail.com>
2023-05-10 16:52:21 +0800

    mod indent

--- leetcode/array/Next Permutation.py
+++ leetcode/array/Next Permutation.py
@@ -14,7 +14,8 @@ class Solution:
             while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
-        nums[i+1:] = reversed(nums[i+1:])
+        nums[i + 1:] = reversed(nums[i + 1:])
+
         return nums
 
 
