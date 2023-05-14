c13e5d86b46f2e3c3a8db834ea7974c1341ced9b
qazw7910 <okmijn7980@gmail.com>
2023-05-14 15:30:51 +0800

    leetcode 543

--- leetcode/tree/Diameter of Binary Tree.py
+++ leetcode/tree/Diameter of Binary Tree.py
@@ -1,40 +1,70 @@
-def main():
-    root = TreeNode(1)
-    root.left = TreeNode(2)
-    root.right = TreeNode(3)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(5)
-    so = Solution()
-    result = so.diameterOfBinaryTree(root)
-    print(result)
+from typing import Optional
 
 
 class TreeNode:
-    def __init__(self, root=0, left=None, right=None):
-        self.root = root
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
         self.left = left
         self.right = right
 
 
+def create_binary_tree(nums):
+    root = TreeNode(nums[0])
+
+    queue = [root]
+
+    i = 1
+
+    while i < len(nums):
+
+        node = queue.pop()
+
+        if nums[i] != None:
+            node.left = TreeNode(nums[i])
+            queue.append(node.left)
+
+        i += 1
+
+        if nums[i] != None and i < len(nums):
+            node.right = TreeNode(nums[i])
+            queue.append(node.right)
+
+        i += 1
+
+    return root
+
+
+def get_node_value(node):
+    if not node:
+        return [None]
+    else:
+        left = get_node_value(node.left)
+        right = get_node_value(node.right)
+
+        return [node.val] + left + right
+
+
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.diameter = 0
 
-        def max_depth(node):
-            if node is None:
+        def find_diameter(node):
+            if not node:
                 return 0
 
-            left_depth = max_depth(node.left)
-            right_depth = max_depth(node.right)
+            left = find_diameter(node.left)
+            right = find_diameter(node.right)
 
-            self.diameter = max(self.diameter, left_depth + right_depth)
+            self.diameter = max(self.diameter, left + right)
 
-            return 1 + max(left_depth, right_depth)
+            return 1 + max(left, right)
 
-        max_depth(root)
+        find_diameter(root)
 
         return self.diameter
 
 
 if __name__ == '__main__':
-    main()
+    root = create_binary_tree(nums=[1, 2, 3, 4, 5])
+    so = Solution()
+    print(so.diameterOfBinaryTree(root))
