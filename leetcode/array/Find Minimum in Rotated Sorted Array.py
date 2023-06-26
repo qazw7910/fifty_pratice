d2eabf0b340b3bf4c298f636bc67fa6b5192515c
陳柏志 <okmijn7980@gmail.com>
2023-06-26 16:19:23 +0800

    add anotation

--- leetcode/array/Find Minimum in Rotated Sorted Array.py
+++ leetcode/array/Find Minimum in Rotated Sorted Array.py
@@ -14,24 +14,25 @@ class Solution:
         left = 0
         right = len(nums) - 1
         # edge case
+        # [0,1,2,4,5,6,7]
         if nums[left] < nums[right]:
             return nums[left]
 
+        # [1]
         if len(nums) == 1:
             return nums[0]
         # binary search
         while left < right:
             mid = (left + right) // 2
-
+            # [4,5,6,7,0,1,2]
             if nums[mid] > nums[mid + 1]:
                 return nums[mid + 1]
-
+            # [5,6,7,0,1,2,4]
             if nums[mid] < nums[mid - 1]:
                 return nums[mid]
-
-            if nums[mid] > nums[left]:
+            # [2,4,5,6,7,0,1]
+            if nums[left] < nums[mid]:
                 left = mid + 1
-
             else:
                 right = mid - 1
 
